0716eb0c4ebddcf74572d80a62c0ea03aeabe191
Jerry Yang <yangjerryonly@yahoo.co.jp>
2023-07-28 16:33:46 +0200

    Implemented spin matrices.

--- skyrsim/__init__.py
+++ skyrsim/__init__.py
@@ -1,4 +1,4 @@
-from .common import pauli, gellmann
+from .common import pauli, gellmann, spin_matrices
 from .model import Model, Potential, QWZModel, BHZModel, TwoBandModel, FourBandModel
 from .bulk import Simulator
 from .edge import EdgeSimulator
--- skyrsim/common.py
+++ skyrsim/common.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Define Pauli matrices for later use
+# Define Pauli matrices
 pauli = np.zeros((4, 2, 2), dtype=np.complex64)
 pauli[0] = np.eye(2)
 pauli[1] = np.array([[0, 1], [1, 0]])
@@ -18,3 +18,19 @@ gellmann[5] = np.array([[0, 0, -1j], [0, 0, 0], [1j, 0, 0]])
 gellmann[6] = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 0]])
 gellmann[7] = np.array([[0, 0, 0], [0, 0, -1j], [0, 1j, 0]])
 gellmann[8] = np.array([[1, 0, 0], [0, 1, 0], [0, 0, -2]]) / np.sqrt(3)
+
+# Define spin matrices
+def spin_matrices(s):
+    if int(s) == s or int(s * 2) == s * 2:
+        N = int(2 * s + 1)
+        S = np.zeros((3, N, N), dtype=np.complex64)
+        m = np.arange(N - 1) - s
+        d = np.sqrt(s*(s+1) - m*(m+1)) / 2
+        S[0] += np.diag(d, k=1)
+        S[0] += np.diag(d, k=-1)
+        S[1] += np.diag(d * -1j, k=1)
+        S[1] += np.diag(d * 1j, k=-1)
+        S[2] = np.diag(s - np.arange(N))
+        return S
+    raise ValueError("Only integer or half-integer spins accepted.")
+
